6592e5cd5424068c75cb757473e72ee8e67584df
Virgian12 <virgiamadhan@gmail.com>
2024-05-06 11:03:15 +0700

    home fix

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,13 +4,13 @@
 <main id="main" class="main">
 
     <div class="pagetitle">
-      <h1>Dashboard</h1>
-      <nav>
+      <h1>Home</h1>
+      {{-- <nav>
         <ol class="breadcrumb">
           <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-          <li class="breadcrumb-item active">Dashboard</li>
+          <li class="breadcrumb-item active">Home</li>
         </ol>
-      </nav>
+      </nav> --}}
     </div><!-- End Page Title -->
 
     <section class="section dashboard">
