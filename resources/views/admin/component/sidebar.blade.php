0743c1b80f38422a5b8e07faf5f826b367cca4e3
Virgian12 <virgiamadhan@gmail.com>
2024-05-16 12:28:21 +0700

    change sidebar, navbar, dsahboard

--- resources/views/admin/component/sidebar.blade.php
+++ resources/views/admin/component/sidebar.blade.php
@@ -1,15 +1,17 @@
   <!-- ======= Sidebar ======= -->
   <aside id="sidebar" class="sidebar">
-
     <ul class="sidebar-nav" id="sidebar-nav">
 
+      <!-- Dashboard Nav -->
       <li class="nav-item">
         <a class="nav-link " href="{{ route('admin.dashboard') }}">
           <i class="bi bi-grid"></i>
           <span>Dashboard</span>
         </a>
-      </li><!-- End Dashboard Nav -->
+      </li>
+      <!-- End Dashboard Nav -->
 
+      <!-- Tables Nav -->
       <li class="nav-item">
         <a class="nav-link collapsed" data-bs-target="#tables-nav" data-bs-toggle="collapse" href="#">
           <i class="bi bi-layout-text-window-reverse"></i><span>Konten</span><i class="bi bi-chevron-down ms-auto"></i>
@@ -17,17 +19,16 @@
         <ul id="tables-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
           <li>
             <a href="{{ route('berita.view') }}">
-              <i class="bi bi-circle"></i><span>Input Artikel</span>
+              <i class="bi bi-circle"></i><span>Data Artikel</span>
             </a>
           </li>
           <li>
             <a href="{{ route('recentpost.view') }}">
-              <i class="bi bi-circle"></i><span>Input Postingan</span>
+              <i class="bi bi-circle"></i><span>Data Postingan</span>
             </a>
           </li>
-<<<<<<< HEAD
+
           
-=======
           <li>
             <a href="{{ route('promo.view')}}">
               <i class="bi bi-circle"></i><span>Data Promo</span>
@@ -38,29 +39,35 @@
               <i class="bi bi-circle"></i><span>Data Kegiatan</span>
             </a>
           </li>
->>>>>>> f3cbf4024004090fd6c2adb7a88b4b0acca08fcc
+
         </ul>
-      </li><!-- End Tables Nav -->
+      </li>
+      <!-- End Tables Nav -->
 
-      <li class="nav-heading">Pages</li>
-
-      <li class="nav-item">
+      <!-- Contact Page Nav -->
+      {{-- <li class="nav-item">
         <a class="nav-link collapsed" href="pages-contact.html">
           <i class="bi bi-envelope"></i>
           <span>Contact</span>
         </a>
-      </li><!-- End Contact Page Nav -->
+      </li> --}}
+      <!-- End Contact Page Nav -->
 
+      <!-- Log Out Page Nav -->
       <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-login.html">
-          <i class="bi bi-box-arrow-in-right"></i>
-          <span>Login</span>
+        <a class="nav-link collapsed" type="submit" href="{{ route('admin.logout') }}">
+          <i class="bi bi-box-arrow-right"></i>
+          <span>Log Out</span>
         </a>
-      </li><!-- End Login Page Nav -->
+      </li>
+      <!-- End Log Out Page Nav -->
 
     </ul>
     
-  </aside><!-- End Sidebar-->
+  </aside>
+  <!-- End Sidebar-->
+
+  
 
   {{-- KODE TIDAK DIPAKAI --}}
   {{-- <li class="nav-item">
@@ -103,3 +110,5 @@
               <i class="bi bi-circle"></i><span>Data Tables</span>
             </a>
           </li> --}}
+
+          {{-- <li class="nav-heading">Pages</li> --}}
